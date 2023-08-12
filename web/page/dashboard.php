fa7a02aa7ab1420144ff1b0623c0105e66f16533
Slyplayer8990 <f.bayhoca@hotmail.com>
2023-08-12 10:53:42 +0300

    "Woo"

--- web/page/dashboard.php
+++ web/page/dashboard.php
@@ -2,9 +2,13 @@
 echo "<meta name='viewport' content='width=device-width', initial-scale=1.0'/>";
 if (isset ($_COOKIE["cloudy"]["user"])) {
     $user = $_COOKIE["cloudy"]["user"];
-    echo "<h1>Welcome to the dashboard, $user                 <button onclick='window.location.href=\"logout.php\"'>Logout</button></h1>";
+    echo "<link href='https://fonts.googleapis.com/css?family=Akaya Kanadaka' rel='stylesheet'>";
+    echo "<body>";
+    echo "<h1>Welcome to the dashboard, $user</h1>";
+    echo "</body>";
     echo "<style>button {background-color: #4CAF50; border: none; color: white; padding: 15px 32px; text-align: right; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer;}</style>";
-    echo "<style>h1 {text-align: center;}</style>";
+    echo "<style>h1 {text-align: left; background-color:#00b71e; color: white;}</style>";
+    echo "<style>body {font-family:Akaya Kanadaka; font-size:32px;} </style>";
 }
 else {
     header("Location: index.php");
