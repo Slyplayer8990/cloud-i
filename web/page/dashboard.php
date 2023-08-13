672c0a227c53df6d3319ef4025986818b1f27d77
Slyplayer8990 <f.bayhoca@hotmail.com>
2023-08-13 11:40:42 +0300

    "Woo"

--- web/page/dashboard.php
+++ web/page/dashboard.php
@@ -4,11 +4,14 @@ if (isset ($_COOKIE["cloudy"]["user"])) {
     $user = $_COOKIE["cloudy"]["user"];
     echo "<link href='https://fonts.googleapis.com/css?family=Akaya Kanadaka' rel='stylesheet'>";
     echo "<body>";
-    echo "<h1>Welcome to the dashboard, $user</h1>";
+    echo "<div style='clear:both;' class='header-container'>";
+    echo "<h1 style='text-align: left; display: inline-block;' class='right-header'>Welcome to the dashboard, $user</h1>";
+    echo "<button style='display: inline-block; float: right;' class='right-header' onclick='window.location.href=\"logout.php\"'>Logout</button>";
+    echo "</div>";
     echo "</body>";
-    echo "<style>button {background-color: #4CAF50; border: none; color: white; padding: 15px 32px; text-align: right; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer;}</style>";
-    echo "<style>h1 {text-align: left; background-color:#00b71e; color: white;}</style>";
+    echo "<style>.header-container {padding: 10px; justify-content: space-between; display:flex}</style>";
     echo "<style>body {font-family:Akaya Kanadaka; font-size:32px;} </style>";
+    echo "<style>.right-header {float: right;}</style>";
 }
 else {
     header("Location: index.php");
